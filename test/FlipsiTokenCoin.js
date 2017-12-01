6278a0f75054baffd8db97285952ff88b965f64b
scopichol <scopichol@gmail.com>
2017-12-02 00:00:56 +0200

    ERC20Complete

--- test/FlipsiTokenCoin.js
+++ test/FlipsiTokenCoin.js
@@ -1,7 +1,7 @@
 const Reverter = require('./helpers/reverter');
 const Asserts = require('./helpers/asserts');
 const FlipsiTokenCoin = artifacts.require('FlipsiTokenCoin');
-
+    
 contract('FlipsiTokenCoin', function(accounts) {
   const reverter = new Reverter(web3);
   afterEach('revert', reverter.revert);
@@ -11,28 +11,33 @@ contract('FlipsiTokenCoin', function(accounts) {
   const holder1 = accounts[1];
   const holder2 = accounts[2];
   //~ const TOTALSUPPLY = parseInt(web3.toWei(20000000));
-  const TOTALTOKENS = 20000000 
+  const TOKENNAME = "Flipsi Token";
+  const TOKENSYMBOL = "FLP";
+  const TOTALTOKENS = 20000000;
+  const TOKENDECIMALS = 8;
+  const TOTALSUPPLY = web3.toBigNumber(TOTALTOKENS).mul(web3.toBigNumber(10).pow(TOKENDECIMALS)) 
+    //~ var balance = new BigNumber('131242344353464564564574574567456');
     
   let token;
-  let TOTALSUPPLY;
+  //~ let TOTALSUPPLY;
     
   before('setup', () => {
     return FlipsiTokenCoin.deployed()
     .then(instance => token = instance)
     .then(() => token.decimals())
-    .then(decimals => TOTALSUPPLY = TOTALTOKENS * Math.pow(10,parseInt(decimals)) ) //* math.pow(10,token.decimals()
-    //~ .then(decimals => TOTALSUPPLY = TOTALTOKENS * Math.pow(10,18) ) //* math.pow(10,token.decimals()
+    //~ .then(decimals => TOTALSUPPLY = web3.toBigNumber(TOTALTOKENS).mul(web3.toBigNumber(10).pow(decimals)) )//* math.pow(10,token.decimals()
+    //~ .then(decimals => )
     .then(reverter.snapshot);
   });
 
   it('should have name, symbol and decimals', () => {
     return Promise.resolve()
     .then(() => token.name())
-    .then(asserts.equal("Flipsi Token"))
+    .then(asserts.equal(TOKENNAME))
     .then(() => token.symbol())
-    .then(asserts.equal("FLP"))
+    .then(asserts.equal(TOKENSYMBOL))
     .then(() => token.decimals())
-    .then(asserts.equal(8))
+    .then(asserts.equal(TOKENDECIMALS))
     ;
   });
   
@@ -45,7 +50,8 @@ contract('FlipsiTokenCoin', function(accounts) {
   it('should totalSupply on owner balance after create', () => {
     return Promise.resolve()
     .then(() => token.balanceOf(OWNER))
-    .then(asserts.equal(TOTALSUPPLY));
+    .then(asserts.equal(TOTALSUPPLY))
+    ;
   });
 
   it('should allow to transfer', () => {
@@ -90,7 +96,7 @@ contract('FlipsiTokenCoin', function(accounts) {
 
   it('should fail on over TOTALSUPPLY balance when transfer', () => {
     return Promise.resolve()
-    .then(() => asserts.throws(token.transfer(holder1, TOTALSUPPLY+1, {from: OWNER})))
+    .then(() => asserts.throws(token.transfer(holder1, TOTALSUPPLY.add(1), {from: OWNER})))
     ;
   });
   
@@ -99,24 +105,6 @@ contract('FlipsiTokenCoin', function(accounts) {
     .then(() => asserts.throws(token.transfer(holder2, 1, {from: holder1})))
     ;
   });
-  
-  it('DEBUG to transfer', () => {
-    const value = 1000;
-    const ts = TOTALSUPPLY;
-
-    return Promise.resolve()
-    .then(() => token.totalSupply())
-    .then(result => console.log('totalSupply', result.toString(10)))
-    .then(() => token.balanceOf(OWNER))
-    .then(result => console.log('balanceOf(OWNER) Before TRANSFER', result.toString(10)))
-    .then(() => token.transfer(holder1, TOTALSUPPLY, {from: OWNER}))
-    .then(() => token.balanceOf(OWNER))
-    .then(result => console.log('balanceOf(OWNER)', result.toString(10)))
-    .then(() => token.balanceOf(holder1))
-    .then(result => console.log('balanceOf(holder1)', result.toString(10)))
-    .then(result => console.log('TOTALSUPPLY',TOTALSUPPLY.toString(10)))
-    .then(result => console.log('TOTALSUPPLY',ts))
-    ;
-  });
+ 
   
 });
