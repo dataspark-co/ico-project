02a3ca85147c959b69637ace796ac3c582896849
Zakharii <contact@dataspark.co>
2017-12-09 22:03:04 +0000

    Separated Pausable Contract into own file

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -2,52 +2,9 @@ pragma solidity ^0.4.18;
 
 import './FlipsiToken.sol';
 import '../math/SafeMath.sol';
+import './Ownable.sol';
+import './Pousable.sol';
 
-/**
- * @title Pausable
- * @dev Base contract which allows to implement an emergency stop mechanism.
- */
-contract Pausable is Ownable {
-    
-  event Pause();
-  
-  event Unpause();
-
-  bool public paused = false;
-
-  /**
-   * @dev modifier to allow actions only when the contract IS paused
-   */
-  modifier whenNotPaused() {
-    require(!paused);
-    _;
-  }
-
-  /**
-   * @dev modifier to allow actions only when the contract IS NOT paused
-   */
-  modifier whenPaused() {
-    require(paused);
-    _;
-  }
-
-  /**
-   * @dev called by the owner to pause, triggers stopped state
-   */
-  function pause() onlyOwner whenNotPaused {
-    paused = true;
-    Pause();
-  }
-
-  /**
-   * @dev called by the owner to unpause, returns to normal state
-   */
-  function unpause() onlyOwner whenPaused {
-    paused = false;
-    Unpause();
-  }
-  
-}
 
 contract CommonSale is Pausable {
 
