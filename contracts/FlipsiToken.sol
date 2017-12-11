62246065f2b2fb101ec8bebdb5b94899d1b8a259
scopichol <scopichol@gmail.com>
2017-12-11 20:33:42 +0200

    block transfer until crowdsale end

--- contracts/FlipsiToken.sol
+++ contracts/FlipsiToken.sol
@@ -228,7 +228,8 @@ contract FlipsiTokenCoin is BurnableToken, Ownable {
      * Overrides ERC20 transfer function with modifier that prevents the
      * ability to transfer tokens until after transfers have been enabled.
      */
-    function transfer(address _to, uint256 _value) public onlyWhenTransferEnabled returns (bool) {
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(transferEnabled);
         return super.transfer(_to, _value);
     }
 
