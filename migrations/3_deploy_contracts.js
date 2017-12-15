fb22528c8f5328f3518970f7226fdcca88be1621
Oleg Scopich <scopichol@gmail.com>
2017-12-15 22:27:28 +0200

    Change deploy for tests

--- migrations/3_deploy_contracts.js
+++ migrations/3_deploy_contracts.js
@@ -3,14 +3,13 @@ var FlipsiPreSale = artifacts.require('./FlipsiPreSale.sol');
 
 module.exports = (deployer, network, accounts) => {
   const start = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1; // one second in the future
-  const durationInMinutes = start + (14400 * 20); // 20 days
+  //const durationInMinutes = 14400 * 20; // 20 days
+  const durationInMinutes = 2; // 20 days
   const rateFlpToEther = new web3.BigNumber(1000);
   const beneficiary = accounts[1];
   const presaleHardcap = web3.toBigNumber(1000000).mul(web3.toBigNumber(10).pow(8));
-  const token = accounts[2];
   deployer.deploy(FlipsiToken)
 	.then(function() {
-    console.log("FlipsiToken.address", FlipsiToken.address);
 		return deployer.deploy(FlipsiPreSale,beneficiary, start, durationInMinutes, rateFlpToEther, presaleHardcap, FlipsiToken.address);
     })
     ;
